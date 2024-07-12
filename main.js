a4ceaa302491ce574886d5c8e1ec5120133cecb3
Joeken10 <joekentafir@gmail.com>
2024-07-12 15:31:36 +0300

    add files

--- main.js
+++ main.js
@@ -4,7 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  // Add the .hidden class to the error modal in the HTML
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
 
+  // Select all the like-glyph elements
+  const hearts = document.querySelectorAll('.like-glyph');
+
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      if (heart.classList.contains('activated-heart')) {
+        // If the heart is full, empty it
+        heart.classList.remove('activated-heart');
+        heart.innerHTML = '&#x2661;';
+      } else {
+        // If the heart is empty, make a server call
+        mimicServerCall()
+          .then(() => {
+            // On successful server response, make the heart full
+            heart.classList.add('activated-heart');
+            heart.innerHTML = '&#x2665;';
+          })
+          .catch((error) => {
+            // On failure, display the error modal
+            errorModal.classList.remove('hidden');
+            document.getElementById('modal-message').innerText = error;
+            // Hide the modal after 3 seconds
+            setTimeout(() => {
+              errorModal.classList.add('hidden');
+            }, 3000);
+          });
+      }
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +57,4 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
